178bcec2f592e7490e1d345a8839e958d7ff6d4c
Paricherh Abedzadeh <Parichehrabedzadeh@gmail.com>
2025-11-01 15:29:41 +0330

    feat : manitainability file for api links just fixed

--- src/Components/Templates/MainPage.jsx
+++ src/Components/Templates/MainPage.jsx
@@ -1,19 +1,18 @@
 import { useEffect, useState } from "react";
 import CoinChart from "../Modules/CoinChart";
+import { getApi } from "../../Services/CryptoApi";
 
 function MainPage() {
   const [coins, setCoins] = useState([]);
   useEffect(() => {
-    fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=layer-1&price_change_percentage=1h&per_page=20&page=1&x-cg-demo-api-key:%20CG-zZb95cAYhBbTfZSFWb1NbszH"
-    )
+    fetch(getApi())
       .then((res) => res.json())
       .then((json) => setCoins(json));
   }, []);
   return (
     <div>
       <p>mainPage</p>
-      <CoinChart coins={coins}/>
+      <CoinChart coins={coins} />
     </div>
   );
 }
