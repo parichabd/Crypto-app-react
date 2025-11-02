3ea060a58b2f98b69813acfe6e7a9a4c5f643905
Paricherh Abedzadeh <Parichehrabedzadeh@gmail.com>
2025-11-02 21:17:43 +0330

    fix : bugs + feat : search input just handeled

--- src/Components/Modules/SearchCurenncy.jsx
+++ src/Components/Modules/SearchCurenncy.jsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
+import { searchApi } from "../../Services/CryptoApi";
+
 function SearchCurenncy({ currency, setCurrency }) {
+  const [search, setSearch] = useState("");
+  const [coins, setCoins] = useState([]);
   const symbols = {
     usd: "$",
     eur: "€",
     jpy: "¥",
   };
+
+  useEffect(() => {
+    if (!search) return;
+    const apiSearch = async () => {
+      const res = await fetch(searchApi(search));
+      const json = await res.json();
+      if (json.coins) setCoins(json.coins);
+    };
+
+    apiSearch();
+  }, [search]);
   return (
     <div>
-      <input type="text" placeholder="Search coin..." />
+      <input
+        type="text"
+        placeholder="Search coin..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
         <option value="usd">USD</option>
         <option value="eur">EUR</option>
--- src/Services/CryptoApi.js
+++ src/Services/CryptoApi.js
@@ -1,7 +1,11 @@
 const MAIN_URL = "https://api.coingecko.com/api/v3";
 const API_KEY = "%20CG-zZb95cAYhBbTfZSFWb1NbszH";
-function getApi(page , currency) {
-  return `${MAIN_URL}/coins/markets?vs_currency=${currency}&category=layer-1&price_change_percentage=1h&per_page=20&page=${page}&x-cg-demo-api-key:${API_KEY}`;
+function getApi(page, currency) {
+  return `${MAIN_URL}/coins/markets?vs_currency=${currency}&category=layer-1&price_change_percentage=1h&per_page=20&page=${page}&x-cg-demo-api-key=${API_KEY}`;
 }
 
-export { getApi };
+function searchApi(query) {
+  return `${MAIN_URL}/search?query=${query}&x-cg-demo-api-key=${API_KEY}`;
+}
+
+export { getApi , searchApi };
