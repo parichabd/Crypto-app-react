19da5577d7649725181a8a5e0f5c6b3ce5741b08
Paricherh Abedzadeh <Parichehrabedzadeh@gmail.com>
2025-11-03 01:01:00 +0330

    feat : chart added by rechart library

--- src/Components/Modules/Chart.jsx
+++ src/Components/Modules/Chart.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { converDta } from "../../Helper/converDta";
 import styles from "./chart.module.css";
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 function Chart({ chart, setChart }) {
   const [type, setType] = useState("prices");
-  console.log(converDta(chart,type));
+  console.log(converDta(chart, type));
   return (
     <div className={styles.container}>
       <span
@@ -14,7 +15,25 @@ function Chart({ chart, setChart }) {
       >
         X
       </span>
-      <div className={styles.chart}></div>
+      <div className={styles.chart}>
+        <div className={styles.graph}>
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart width={400} height={400} data={converDta(chart, type)}>
+              <Line
+                trye="monotone"
+                dataKey={type}
+                stroke="#3874ff"
+                strokeWidth="2px"
+              />
+              <CartesianGrid stroke="#404042" />
+              <YAxis dataKey={type} domain={["auto" , "auto"]}/>
+              <XAxis dataKey={type} hide/>
+              <Legend/>
+               <Tooltip/>
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
     </div>
   );
 }
